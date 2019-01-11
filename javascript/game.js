cacf90895aac6ae832244177f931f73f56da5f78
Jimin Huh <jiminhuh95@gmail.com>
2019-01-10 19:01:26 -0600

    added more styles and added a reset button

---
+++ javascript/game.js
@@ -0,0 +1,6 @@
+//When Page loads random Number is generated 1 ~ 65.
+//When Page loads each crystal will be given a random value between 1 ~ 15.
+//clicking each crystal will add to "Your score is" value
+//If you hit the random number value, You win, then add 1 to Wins.
+//If you go over the number, You lose, then add 1 to Loss.
+//Click reset button to play again.
